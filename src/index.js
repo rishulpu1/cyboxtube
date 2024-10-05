583b797e9f2f7c686c0e3e4ea79858770b227ee7
Rajneesh Kumar <rishulpu1@gmail.com>
2024-10-05 13:17:35 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running on Port:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error", err);
+  });
